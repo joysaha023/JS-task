fe45103c79e15fa6d5135395317aed9ae5e8a42f
Joy <github.joysaha@gmail.com>
2024-01-30 20:55:39 +0600

    string comparision

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
-const country = 'Bangladesh';
-const division = "Dhaka";
-const district = `B-baria`;
-const thana = new String('Akhaura');
+// const country = 'Bangladesh';
+// const division = "Dhaka";
+// const district = `B-baria`;
+// const thana = new String('Akhaura');
 
 // console.log(district);
 
-const numbers = [54, 98, 78, 21, 65];
+// const numbers = [54, 98, 78, 21, 65];
 // console.log(numbers.length);
 // console.log(numbers[0]);
-numbers[1] = 988;
-console.log(numbers);
+// numbers[1] = 988;
+// console.log(numbers);
 
-const capital = 'Dhaka';
+// const capital = 'Dhaka';
 // console.log(capital.length);
 // console.log(capital[2]);
-capital[1] = 'B';
-console.log(capital);
+// capital[1] = 'B';
+// console.log(capital);
+
+
+// const school = 'Any Govt School'
+// console.log(school);
+// console.log(school.toLowerCase());
+// console.log(school.toUpperCase());
+
+// const subject = 'Math';
+// const book = 'math';
+
+// if (subject.toLowerCase() === book.toLowerCase()){
+//     console.log('jodi mile r ki');
+// }
+// else{
+//     console.log('jodi na mile taile');
+// }
+
+const drink = 'water ';
+const liquid = '  water';
+
+if(drink.trim() === liquid.trim()){
+    console.log('jodi trim hoi');
+}
+else{
+    console.log('jodi trim na hoi r ki');
+}
