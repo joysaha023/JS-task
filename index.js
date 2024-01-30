c9a363da4e343bb397b0badeea7afd82648b8766
Joy <github.joysaha@gmail.com>
2024-01-30 20:26:32 +0600

    coding 2

--- index.js
+++ index.js
@@ -6,5 +6,13 @@ const thana = new String('Akhaura');
 // console.log(district);
 
 const numbers = [54, 98, 78, 21, 65];
-console.log(numbers.length);
-console.log(numbers[0]);
+// console.log(numbers.length);
+// console.log(numbers[0]);
+numbers[1] = 988;
+console.log(numbers);
+
+const capital = 'Dhaka';
+// console.log(capital.length);
+// console.log(capital[2]);
+capital[1] = 'B';
+console.log(capital);
