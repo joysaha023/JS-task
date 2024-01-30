7e47f3cabcf91acf899fc8181f081a35cf9d4c16
Joy <github.joysaha@gmail.com>
2024-01-30 20:17:43 +0600

    coding

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("for github test");
+const country = 'Bangladesh';
+const division = "Dhaka";
+const district = `B-baria`;
+const thana = new String('Akhaura');
+
+// console.log(district);
+
+const numbers = [54, 98, 78, 21, 65];
+console.log(numbers.length);
+console.log(numbers[0]);
